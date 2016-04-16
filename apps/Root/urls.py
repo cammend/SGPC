bdcaf5222b02c890c3d654788eae96a0e352fcad
Carlos Caal Mendoza <cammend.net@gmail.com>
2016-04-16 13:18:24 -0600

    Modificando app Root
    se están agregando el index de la página para gestión del root

--- apps/Root/urls.py
+++ apps/Root/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url, patterns
+from .views import root
+from apps.Estado.views import addEstadoDepto
 
 urlpatterns = patterns('',
-	#url(r'^$', index),
+	url(r'^$', root),
+	url(r'^asociar/$', addEstadoDepto),
 )
