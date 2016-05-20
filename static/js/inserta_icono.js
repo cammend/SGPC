5aac4792783d005f0d2e311984d4f2c6fdf15a96
¨Brandon <brandonleo1993@gmail.com>
2016-05-20 17:55:44 -0600

    cambios

--- static/js/inserta_icono.js
+++ static/js/inserta_icono.js
@@ -3,6 +3,17 @@ $('#form_inicio p:nth-child(3)').append('<i class="fa fa-user"></i>');
 
 $('#form_inicio p:nth-child(4)').append('<i class="fa fa-key"></i>');
 
+$('#id_username').attr({placeholder : "Usuario"});
+$('#id_password').attr({placeholder : "Ingrese Contraseña"});
+
+$('#id_alias').attr({placeholder : "Alias"});
+$('#id_nombres').attr({placeholder : "Ingrese sus Nombres"});
+$('#id_apellidos').attr({placeholder : "Ingrese sus apellidos"});
+$('#id_correo').attr({placeholder : "Ingrese su correo Electronico"});
+$('#id_password1').attr({placeholder : "Ingrese Contraseña"});
+$('#id_password2').attr({placeholder : "Repita Contraseña"});
+
+
 $('#id_fecha').datepicker({
 				changeMonth:true,
 				changeYear:true,
